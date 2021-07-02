9153debf3a1766c254464e63bf0a027045eebdd9
Yusupov-droid <jamnum9813@gmail.com>
2021-07-01 23:51:08 -0500

    Build v5.3.0 for Release

--- parser/parser.mjs
+++ parser/parser.mjs
@@ -6,7 +6,7 @@ import prettier from 'prettier';
 
 const file_max = './src/iconset.js'
 
-const address = 'https://github.com/FortAwesome/Font-Awesome/raw/5.2.0/advanced-options/metadata/icons.yml'
+const address = 'https://github.com/FortAwesome/Font-Awesome/raw/5.3.0/advanced-options/metadata/icons.yml'
 
 
 let parser = async () => {
